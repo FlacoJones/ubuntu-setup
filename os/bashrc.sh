ea9c993427171865dc524715e76bbde4554720ed
Flaco Jones <alo@alo.attlocal.net>
2025-10-13 17:03:53 -0500

    adds command to source

--- os/bashrc.sh
+++ os/bashrc.sh
@@ -15,5 +15,8 @@ add_to_bashrc() {
 add_to_bashrc "alias c='clear'"
 add_to_bashrc "stty intr ^E"
 
-# Source the updated .bashrc
-source ~/.bashrc
+echo ""
+echo "To apply changes to your current shell, run:"
+echo "source ~/.bashrc"
+echo ""
+echo "Or simply open a new terminal session."
