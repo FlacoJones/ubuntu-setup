b62cf5c3285bd2257bf02828d573c6af738c0bb3
Flaco Jones <alo@alo.attlocal.net>
2025-10-13 16:54:10 -0500

    updates terminal keybindings

--- terminal.sh
+++ terminal.sh
@@ -3,3 +3,6 @@ stty intr ^E
 gsettings set org.gnome.desktop.interface can-change-accels true
 gsettings set org.gnome.Terminal.Legacy.Keybindings:/org/gnome/terminal/legacy/keybindings/ copy '<Primary>c'
 gsettings set org.gnome.Terminal.Legacy.Keybindings:/org/gnome/terminal/legacy/keybindings/ paste '<Primary>v'
+gsettings set org.gnome.Terminal.Legacy.Keybindings:/org/gnome/terminal/legacy/keybindings/ select-all '<Primary>a'
+gsettings set org.gnome.Terminal.Legacy.Keybindings:/org/gnome/terminal/legacy/keybindings/ find '<Primary>f'
+gsettings set org.gnome.Terminal.Legacy.Keybindings:/org/gnome/terminal/legacy/keybindings/ new-tab '<Primary>t'
