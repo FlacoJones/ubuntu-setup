7dcc7a341fe2f65e868dacc15751a85e5e08c033
Flaco Jones <alo@alo.attlocal.net>
2025-10-13 17:02:02 -0500

    makes bashrc idempotent

--- os/bashrc.sh
+++ os/bashrc.sh
@@ -1,3 +1,19 @@
-echo "alias c='clear'" >> ~/.bashrc
-echo "stty intr ^E" >> ~/.bashrc
+#!/bin/bash
+
+# Function to add line to .bashrc only if it doesn't already exist
+add_to_bashrc() {
+    local line="$1"
+    if ! grep -Fxq "$line" ~/.bashrc; then
+        echo "$line" >> ~/.bashrc
+        echo "Added: $line"
+    else
+        echo "Already exists: $line"
+    fi
+}
+
+# Add configurations to .bashrc
+add_to_bashrc "alias c='clear'"
+add_to_bashrc "stty intr ^E"
+
+# Source the updated .bashrc
 source ~/.bashrc
